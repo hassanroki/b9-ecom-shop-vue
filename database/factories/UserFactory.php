edb30d492bcdb9b9a4eb1e2582e4254025aa14b8
hassanroki <mdrokibulhassan786@gmail.com>
2026-06-29 13:14:10 +0600

    Seed Complete

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -41,8 +41,18 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the user is a store admin.
+     */
+    public function admin(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'role' => 'admin',
         ]);
     }
 
@@ -51,7 +61,7 @@ class UserFactory extends Factory
      */
     public function withTwoFactor(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'two_factor_secret' => encrypt('secret'),
             'two_factor_recovery_codes' => encrypt(json_encode(['recovery-code-1'])),
             'two_factor_confirmed_at' => now(),
